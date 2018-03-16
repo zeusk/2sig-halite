7598461392ca458aeafb31f30fc5da684f10747a
Shantanu Gupta <sg3273@drexel.edu>
2018-03-16 16:27:46 -0400

    [Policy] Add doomed policy
     Changes to be committed:
    	modified:   MyBot.py

--- MyBot.py
+++ MyBot.py
@@ -9,7 +9,7 @@ from hlt.constants import *
 from hlt.entity import Position
 
 # GAME START
-game = hlt.Game("HunterKiller")
+game = hlt.Game("Mu - 2sigma")
 turn = 0
 rush_policy = False
 rush_policy_num_players_max = 2
@@ -29,6 +29,10 @@ while True:
         dplayers = hlt.geom.pp_dist(e_cent, m_cent)
         if nplayers <= rush_policy_num_players_max and dplayers <= rush_policy_dis_players_max:
             rush_policy = True
+
+    doomed = False
+    if (nplayers == 4 and len(gmap.en_ships()) > len(gmap.my_ships())) and (len([e for e in gmap.unowned_planets() + gmap.my_uplanets() if e.remaining_resources > 0 ]) == 0):
+        doomed = True
 
     # Execute out strategy
     #HANDLE ATKS AT T=0
@@ -108,127 +112,145 @@ while True:
         if s not in first_targ:
             first_targ[s] = e
 
-        if type(e) == hlt.entity.Planet and rem_dock[e] > 0:
-            if s.can_dock(e):
-                nav_cmd = s.dock(e)
-                rem_dock[e] -= 1
-                cmds.append(nav_cmd)
-            else:
-                nav_cmd, move = helper.nav(s,s.closest_pt_to(e), gmap, None,move_table)
-                if nav_cmd:
+        if not doomed:
+            if type(e) == hlt.entity.Planet and rem_dock[e] > 0:
+                if s.can_dock(e):
+                    nav_cmd = s.dock(e)
                     rem_dock[e] -= 1
                     cmds.append(nav_cmd)
-                if move:
-                    move_table[s] = move
-            unassigned.discard(s)
-        elif type(e) == hlt.entity.Ship:
+                else:
+                    nav_cmd, move = helper.nav(s,s.closest_pt_to(e), gmap, None,move_table)
+                    if nav_cmd:
+                        rem_dock[e] -= 1
+                        cmds.append(nav_cmd)
+                    if move:
+                        move_table[s] = move
+                unassigned.discard(s)
+            elif type(e) == hlt.entity.Ship:
+                # WITHIN POTENTIAL ATTACK RANGE
+                if s.dist_to(e) <= WEAPON_RADIUS + 2*MAX_SPEED:
+                    if not doomed:
+                        # ATTACK
+                        if s in has_atked:
+                            nav_cmd, move = helper.nav(s,s.closest_pt_to(e,6),gmap,None,move_table)
+                        else:
+                            nav_cmd, move = helper.nav(s,s.closest_pt_to(e),gmap,None,move_table)
+                        if move:
+                            atk_pos = Position(move.p2)
+                        else:
+                            atk_pos = Position(s.loc, s.loc + Point.polar(MAX_SPEED,s.angle_to(e)))
+                        atk_ens = [t for t in gmap.en_uships() if t.dist_to(atk_pos) <= WEAPON_RADIUS + MAX_SPEED]
+                        atk_frs = [t for t in gmap.my_uships() if t.dist_to(atk_pos) <= MAX_SPEED+2]
+                        if len(atk_frs) > len(atk_ens):
+                            if nav_cmd:
+                                en_ship_assigned[e] -= 1
+                                cmds.append(nav_cmd)
+                            if move:
+                                move_table[s] = move
+                            unassigned.discard(s)
+                            continue
 
-            # WITHIN POTENTIAL ATTACK RANGE
-            if s.dist_to(e) <= WEAPON_RADIUS + 2*MAX_SPEED:
-                # ATTACK
-                if s in has_atked:
-                    nav_cmd, move = helper.nav(s,s.closest_pt_to(e,6),gmap,None,move_table)
-                else:
-                    nav_cmd, move = helper.nav(s,s.closest_pt_to(e),gmap,None,move_table)
-                if move:
-                    atk_pos = Position(move.p2)
-                else:
-                    atk_pos = Position(s.loc, s.loc + Point.polar(MAX_SPEED,s.angle_to(e)))
-                atk_ens = [t for t in gmap.en_uships() if t.dist_to(atk_pos) <= WEAPON_RADIUS + MAX_SPEED]
-                atk_frs = [t for t in gmap.my_uships() if t.dist_to(atk_pos) <= MAX_SPEED+2]
-                if len(atk_frs) > len(atk_ens):
+                        # OTHER OPTIONS
+                        en_dships = sorted(gmap.en_dships(),key=lambda t:s.dist_to(t))
+                        my_dships = sorted(gmap.my_dships(),key=lambda t:e.dist_to(t))
+                        my_dship = my_dships[0] if my_dships else None
+                        en_dship = en_dships[0] if en_dships else None
+
+                        if time.process_time() - start_time > 1.9:
+                            logging.info("TOOK WAY TOO MUCH TIME")
+                            break
+
+                        # HARASS
+                        if en_dship != None and (my_dship == None or s.dist_to(en_dship)+7*(len(gmap.all_players()) - 1) < e.dist_to(my_dship)) and en_ship_assigned[en_dship] > 0:
+                            chasers = [t for t in gmap.en_uships() if s.dist_to(t) <= 2*MAX_SPEED+WEAPON_RADIUS]
+                            nav_cmd, move = helper.harass_nav(s,en_dship,gmap,None,move_table,enemies=chasers)
+                            if nav_cmd:
+                                cmds.append(nav_cmd)
+                                if move:
+                                    move_table[s] = move
+                                unassigned.discard(s)
+                                #logging.info("{} harass {}".format(s,en_dship))
+                                continue
+
+                        if time.process_time() - start_time > 1.9:
+                            logging.info("TOOK WAY TOO MUCH TIME")
+                            break
+
+                        # DEFEND
+                        if my_dship != None:
+                            def_frns = [t for t in gmap.my_uships() if t.dist_to(my_dship) <= MAX_SPEED + 3]
+                            def_ens = [t for t in gmap.en_uships() if t.dist_to(my_dship) <= MAX_SPEED + WEAPON_RADIUS]
+                            def_dfrns = [t for t in gmap.my_dships() if t.dist_to(my_dship) <= 3]
+                            if e.dist_to(my_dship) <= WEAPON_RADIUS + MAX_SPEED and len(def_frns+def_dfrns) >= len(def_ens):
+                                #logging.info("{} defend {}".format(s,my_dship))
+                                pos = Position(my_dship.loc + Point.polar(.500001, my_dship.angle_to(e)+90))
+                                nav_cmd, move = helper.nav(s,pos,gmap,None,move_table)
+                            else:
+                                enemies = [t for t in gmap.en_uships() if s.dist_to(t)<= MAX_SPEED*2 + WEAPON_RADIUS]
+                                enemies = sorted(enemies,key=lambda t:s.dist_to(t))
+                                en_cent = helper.cent_of_mass(enemies)
+                                if len(enemies) == 0:
+                                    logging.info("{} defend {}".format(s,e))
+                                d = WEAPON_RADIUS+len(gmap.all_players())-2 - (s.dist_to(enemies[0]) - MAX_SPEED)
+                                dv = Point.polar(d, s.angle_to(en_cent))
+                                pos = Position(s.loc - dv)
+                                nav_cmd, move = helper.nav(s,pos,gmap,None,move_table)
+
+                            if nav_cmd:
+                                cmds.append(nav_cmd)
+                            if move:
+                                move_table[s] = move
+                            unassigned.discard(s)
+                            continue
+
+                        if time.process_time() - start_time > 1.9:
+                            logging.info("TOOK WAY TOO MUCH TIME")
+                            break
+
+                    #FLEE
+                    enemies = [t for t in gmap.en_uships() if s.dist_to(t)<= MAX_SPEED*2 + WEAPON_RADIUS]
+                    enemies = sorted(enemies,key=lambda t:s.dist_to(t))
+                    en_cent = helper.cent_of_mass(enemies)
+                    dv = Point.polar(MAX_SPEED, s.angle_to(en_cent))
+                    pos = Position(s.loc - dv)
+                    nav_cmd, move = helper.nav(s,pos,gmap,None,move_table)
+                    if nav_cmd:
+                        cmds.append(nav_cmd)
+                    if move:
+                        move_table[s] = move
+                    unassigned.discard(s)
+
+                # OUTSIDE POTENTIAL ATTACK RANGE
+                elif en_ship_assigned[e] > 0:
+                    if time.process_time() - start_time > 1.9:
+                        logging.info("TOOK WAY TOO MUCH TIME")
+                        break
+                    nav_cmd, move = helper.nav(s,e,gmap,None,move_table)
                     if nav_cmd:
                         en_ship_assigned[e] -= 1
                         cmds.append(nav_cmd)
                     if move:
                         move_table[s] = move
                     unassigned.discard(s)
-                    continue
-
-                # OTHER OPTIONS
-                en_dships = sorted(gmap.en_dships(),key=lambda t:s.dist_to(t))
-                my_dships = sorted(gmap.my_dships(),key=lambda t:e.dist_to(t))
-                my_dship = my_dships[0] if my_dships else None
-                en_dship = en_dships[0] if en_dships else None
-
-                if time.process_time() - start_time > 1.9:
-                    logging.info("TOOK WAY TOO MUCH TIME")
-                    break
-
-                # HARASS
-                if en_dship != None and (my_dship == None or s.dist_to(en_dship)+7*(len(gmap.all_players()) - 1) < e.dist_to(my_dship)) and en_ship_assigned[en_dship] > 0:
-                    chasers = [t for t in gmap.en_uships() if s.dist_to(t) <= 2*MAX_SPEED+WEAPON_RADIUS]
-                    nav_cmd, move = helper.harass_nav(s,en_dship,gmap,None,move_table,enemies=chasers)
-                    if nav_cmd:
-                        cmds.append(nav_cmd)
-                        if move:
-                            move_table[s] = move
-                        unassigned.discard(s)
-                        #logging.info("{} harass {}".format(s,en_dship))
-                        continue
-
-                if time.process_time() - start_time > 1.9:
-                    logging.info("TOOK WAY TOO MUCH TIME")
-                    break
-
-                # DEFEND
-                if my_dship != None:
-                    def_frns = [t for t in gmap.my_uships() if t.dist_to(my_dship) <= MAX_SPEED + 3]
-                    def_ens = [t for t in gmap.en_uships() if t.dist_to(my_dship) <= MAX_SPEED + WEAPON_RADIUS]
-                    def_dfrns = [t for t in gmap.my_dships() if t.dist_to(my_dship) <= 3]
-                    if e.dist_to(my_dship) <= WEAPON_RADIUS + MAX_SPEED and len(def_frns+def_dfrns) >= len(def_ens):
-                        #logging.info("{} defend {}".format(s,my_dship))
-                        pos = Position(my_dship.loc + Point.polar(.500001, my_dship.angle_to(e)+90))
-                        nav_cmd, move = helper.nav(s,pos,gmap,None,move_table)
-                    else:
-                        enemies = [t for t in gmap.en_uships() if s.dist_to(t)<= MAX_SPEED*2 + WEAPON_RADIUS]
-                        enemies = sorted(enemies,key=lambda t:s.dist_to(t))
-                        en_cent = helper.cent_of_mass(enemies)
-                        if len(enemies) == 0:
-                            logging.info("{} defend {}".format(s,e))
-                        d = WEAPON_RADIUS+len(gmap.all_players())-2 - (s.dist_to(enemies[0]) - MAX_SPEED)
-                        dv = Point.polar(d, s.angle_to(en_cent))
-                        pos = Position(s.loc - dv)
-                        nav_cmd, move = helper.nav(s,pos,gmap,None,move_table)
-
-                    if nav_cmd:
-                        cmds.append(nav_cmd)
-                    if move:
-                        move_table[s] = move
-                    unassigned.discard(s)
-                    continue
-
-                if time.process_time() - start_time > 1.9:
-                    logging.info("TOOK WAY TOO MUCH TIME")
-                    break
-
-                #Potential flee logic.
-                # if (len(gmap.en_ships()) > len(gmap.my_ships())) and (len(gmap.rich_planet()) == 0)
-                #FLEE
-                enemies = [t for t in gmap.en_uships() if s.dist_to(t)<= MAX_SPEED*2 + WEAPON_RADIUS]
+        else:
+            enemies = [t for t in gmap.en_uships() if s.dist_to(t)<= MAX_SPEED*2 + WEAPON_RADIUS]
+            if len(enemies):
                 enemies = sorted(enemies,key=lambda t:s.dist_to(t))
                 en_cent = helper.cent_of_mass(enemies)
                 dv = Point.polar(MAX_SPEED, s.angle_to(en_cent))
                 pos = Position(s.loc - dv)
-                nav_cmd, move = helper.nav(s,pos,gmap,None,move_table)
-                if nav_cmd:
-                    cmds.append(nav_cmd)
-                if move:
-                    move_table[s] = move
-                unassigned.discard(s)
-
-            # OUTSIDE POTENTIAL ATTACK RANGE
-            elif en_ship_assigned[e] > 0:
-                if time.process_time() - start_time > 1.9:
-                    logging.info("TOOK WAY TOO MUCH TIME")
-                    break
-                nav_cmd, move = helper.nav(s,e,gmap,None,move_table)
-                if nav_cmd:
-                    en_ship_assigned[e] -= 1
-                    cmds.append(nav_cmd)
-                if move:
-                    move_table[s] = move
-                unassigned.discard(s)
+            else:
+                corners = []
+                top_left = Position(Point(0,0))
+                top_right = Position(Point(gmap.width,0))
+                bottom_left = Position(Point(0,gmap.height))
+                bottom_right = Position(Point(gmap.width,gmap.height))
+                corners.append(top_left)
+                corners.append(top_right)
+                corners.append(bottom_left)
+                corners.append(bottom_right)
+                sorted(corners, key=lambda t:s.dist_to(t))
+                pos = corners[0]
 
     for s in first_targ:
         if time.process_time() - start_time > 1.9:
